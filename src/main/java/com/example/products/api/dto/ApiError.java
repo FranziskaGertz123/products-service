165f043b453d77080743ad7617030e73b05c24f1
FranziskaGertz123 <gertzfranziska7@gmail.com>
2025-12-18 17:02:16 +0100

    fix apierror

--- src/main/java/com/example/products/api/dto/ApiError.java
+++ src/main/java/com/example/products/api/dto/ApiError.java
@@ -1,5 +1,13 @@
 package com.example.products.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApiError {
     private String message;
 }
+
--- src/main/java/com/example/products/application/dao/Ingredient.java
+++ src/main/java/com/example/products/application/dao/Ingredient.java
@@ -1,6 +1,8 @@
 package com.example.products.application.dao;
 
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -10,7 +12,10 @@ import java.math.BigDecimal;
 public class Ingredient {
     private String name;
     private BigDecimal usage;
+
+    @Enumerated(EnumType.STRING)
     private Unit unit;
+
     private BigDecimal price;
 }
 
